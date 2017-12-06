0703e90e7fbb32acb227c1835ee57841e189c8c3
Darian Plumb <dplumb@bitwise.io>
2017-12-06 15:06:39 -0600

    Implement account fetch routes
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

---
+++ rest_api/db/accounts_query.py
@@ -0,0 +1,64 @@
+# Copyright 2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+import rethinkdb as r
+from rethinkdb.errors import ReqlNonExistenceError
+
+from api.errors import ApiBadRequest
+
+from db import blocks_query
+
+
+async def fetch_all_account_resources(conn):
+    return await r.table('accounts')\
+        .filter((blocks_query.latest_block_num() >= r.row['start_block_num'])
+                & (blocks_query.latest_block_num() < r.row['end_block_num']))\
+        .map(lambda account: account.merge(
+            {'publicKey': account['public_key']}))\
+        .map(lambda account: (account['label'] == "").branch(
+            account.without('label'), account))\
+        .map(lambda account: (account['description'] == "").branch(
+            account.without('description'), account))\
+        .without('public_key', 'delta_id',
+                 'start_block_num', 'end_block_num')\
+        .coerce_to('array').run(conn)
+
+
+async def fetch_account_resource(conn, public_key, auth_key):
+    try:
+        return await r.table('accounts')\
+            .get_all(public_key, index='public_key')\
+            .max('start_block_num')\
+            .merge({'publicKey': r.row['public_key']})\
+            .do(lambda account: (r.expr(auth_key).eq(public_key)).branch(
+                account.merge(_fetch_email(public_key)), account))\
+            .do(lambda account: (account['label'] == "").branch(
+                account.without('label'), account))\
+            .do(lambda account: (account['description'] == "").branch(
+                account.without('description'), account))\
+            .without('public_key', 'delta_id',
+                     'start_block_num', 'end_block_num')\
+            .run(conn)
+    except ReqlNonExistenceError:
+        raise ApiBadRequest(
+            "Bad Request: "
+            "No account with the public key {} exists".format(public_key))
+
+
+def _fetch_email(public_key):
+    return r.table('auth')\
+        .get_all(public_key, index='public_key')\
+        .pluck('email')\
+        .coerce_to('array')[0]
---
+++ rest_api/db/blocks_query.py
@@ -0,0 +1,28 @@
+# Copyright 2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+import rethinkdb as r
+from rethinkdb import ReqlNonExistenceError
+
+from api.errors import ApiInternalError
+
+
+def latest_block_num():
+    try:
+        return r.table('blocks')\
+            .max(index='block_num')\
+            .get_field('block_num')
+    except ReqlNonExistenceError:
+        raise ApiInternalError('Internal Error: No block data found in state')
