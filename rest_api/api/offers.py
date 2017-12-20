dcb332640876c99e6e7fdf68baf3848e250ae7a1
Zac Delventhal <zac@bitwise.io>
2017-12-20 14:52:39 -0500

    Remove response bodies from PATCH routes
    Sending back proper RESTful responses to these routes introduces
    race conditions between the REST API and ledger sync. A proper
    solution to this problem will likely require a "big" solution
    including possibly some architectural changes. Insead, the RESTful
    approach is being abandoned for these two routes, with simply
    200 responses sent back instead of full resources.
    
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- rest_api/api/offers.py
+++ rest_api/api/offers.py
@@ -94,15 +94,6 @@ async def accept_offer(request, offer_id):
     offer = await offers_query.fetch_offer_resource(
         request.app.config.DB_CONN, offer_id)
 
-    keys = ['source', 'target']
-    holdings = await fetch_holdings([
-        request.json.get(k) for k in keys if request.json.get(k) is not None
-    ]).run(request.app.config.DB_CONN)
-
-    holdings_dict = {
-        k: h for h in holdings for k in keys if request.json.get(k) == h['id']
-    }
-
     signer = await common.get_signer(request)
     batches, batch_id = transaction_creation.accept_offer(
         txn_key=signer,
@@ -121,10 +112,7 @@ async def accept_offer(request, offer_id):
 
     await messaging.check_batch_status(request.app.config.VAL_CONN, batch_id)
 
-    updated_holdings_dict = _calculate_accept_offer(
-        holdings_dict, offer, request.json['count'])
-
-    return response.json(updated_holdings_dict)
+    return response.json('')
 
 
 @OFFERS_BP.patch('offers/<offer_id>/close')
@@ -144,22 +132,7 @@ async def close_offer(request, offer_id):
 
     await messaging.check_batch_status(request.app.config.VAL_CONN, batch_id)
 
-    updated_offer = await offers_query.fetch_offer_resource(
-        request.app.config.DB_CONN, offer_id)
-    updated_offer['status'] = "CLOSED"
-
-    return response.json(updated_offer)
-
-
-def _calculate_accept_offer(holdings_dict, offer, count):
-    input_quantity = offer['sourceQuantity'] * count
-    holdings_dict['target']['quantity'] += input_quantity
-
-    if holdings_dict.get('source'):
-        output_quantity = offer['targetQuantity'] * count
-        holdings_dict['source']['quantity'] -= output_quantity
-
-    return holdings_dict
+    return response.json('')
 
 
 def _create_offer_dict(body, public_key):
