beab0d35bfbbd3654f21cbc8c9f2e629e909db0c
Zac Delventhal <zac@bitwise.io>
2017-12-20 20:52:10 -0500

    Validate offer and request buttons on Asset pages
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- sawbuck_app/src/views/asset_list.js
+++ sawbuck_app/src/views/asset_list.js
@@ -19,21 +19,30 @@
 const m = require('mithril')
 const _ = require('lodash')
 
+const acct = require('../services/account')
 const layout = require('../components/layout')
 const api = require('../services/api')
 const { createOffer } = require('./create_offer_modal')
 
-const offerButton = (name, key = 'source') => {
+const offerButton = (name, disabled, key = 'source') => {
   const label = key === 'target' ? 'Request' : 'Offer'
   const onclick = key === 'target'
     ? () => createOffer(null, name)
     : () => createOffer(name)
 
-  return m('button.btn.btn-outline-primary.mr-3', { onclick }, label)
+  return m(`button.btn.btn-outline-${disabled ? 'secondary' : 'primary'}.mr-3`,
+           { onclick, disabled },
+           label)
 }
 
-const assetRow = asset => {
+const assetRowMapper = account => asset => {
   const safeName = window.encodeURI(asset.name)
+  const offerDisabled = !account ||
+    !account.quantities[asset.name] ||
+    (asset.rules.find(({ type }) => type === 'NOT_TRANSFERABLE') &&
+      !asset.owners.find(owner => owner === account.publicKey))
+  const requestDisabled = !account
+
   return m('.row.mb-5', [
     m('.col-md-8', [
       layout.row(m('a.h5', {
@@ -43,8 +52,8 @@ const assetRow = asset => {
       layout.row(m('.text-muted', asset.description))
     ]),
     m('.col-md-4.mt-3', [
-      offerButton(asset.name),
-      offerButton(asset.name, 'target')
+      offerButton(asset.name, offerDisabled),
+      offerButton(asset.name, requestDisabled, 'target')
     ])
   ])
 }
@@ -55,8 +64,15 @@ const assetRow = asset => {
  */
 const AssetListPage = {
   oninit (vnode) {
-    api.get(`assets`)
-      .then(assets => { vnode.state.assets = assets })
+    Promise.all([ acct.getUserAccount(), api.get(`assets`) ])
+      .then(([ account, assets ]) => {
+        vnode.state.assets = assets
+
+        if (account) {
+          const quantities = acct.getAssetQuantities(account)
+          vnode.state.account = _.assign({ quantities }, account)
+        }
+      })
   },
 
   view (vnode) {
@@ -66,7 +82,7 @@ const AssetListPage = {
       layout.title('Assets Available'),
       m('.container.mt-6',
         assets.length > 0
-          ? assets.map(assetRow)
+          ? assets.map(assetRowMapper(vnode.state.account))
           : m('.text-center',
               m('em', 'there are currently no available assets')))
     ]
